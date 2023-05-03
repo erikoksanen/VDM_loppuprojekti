9238aff192ce9d446e7931efa47ab639aaab4f88
erikoksanen <126602204+erikoksanen@users.noreply.github.com>
2023-05-03 17:34:13 +0300

    Placeholder Dropdown vielä alhaalla

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// JavaScript code here
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
