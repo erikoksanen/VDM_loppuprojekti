cbc62c13cc5e729bff57abcac75d18bb57655196
Anatudek <anttikomulainen10@gmail.com>
2023-05-15 13:22:41 +0300

    Siistitty fade

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//const osWrappers = document.getElementsByClassName("os__wrapper");
+
 
 function uuidv4() {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
@@ -18,8 +18,6 @@ const fadelist = [fade1, fade2, fade3, fade4]
 console.log(fade1)
 window.addEventListener("scroll", ()=>{
   const windowHeight = innerHeight
-  //console.log(window.scrollY)
-  //console.log(fade1.getBoundingClientRect().top)
   for (fade of fadelist) {
     if (fade.getBoundingClientRect().top > windowHeight/4 && fade.getBoundingClientRect().top < windowHeight/3) {
       fade.classList.add("fade-on")
