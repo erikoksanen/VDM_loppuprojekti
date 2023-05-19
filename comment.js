52cae6e7fc9f88d7acc73a553647dedcfef9f733
helmitee <130982018+helmitee@users.noreply.github.com>
2023-05-19 14:25:04 +0300

    kommenttikenttä toimii

--- comment.js
+++ comment.js
@@ -3,20 +3,27 @@ const kommentit = document.getElementsByClassName("kommentit")[0]
 const nappi = document.getElementsByClassName("nappula")[0]
 
 function addComment() {
-  const input = kommenttikenttä.value
-  const teksti = document.createTextNode(input)
-  const li = document.createElement("li")
-  const textdiv = document.createElement("div")
-  const deleteButton = document.createElement("button")
-  deleteButton.innerHTML = "Poista"
-  textdiv.appendChild(teksti)
-  li.appendChild(textdiv)
-  li.appendChild(deleteButton)
-  kommentit.appendChild(li)
-  deleteButton.onclick = deleteComment(deleteButton.parentElement)
-
-  kommenttikenttä.value = "";
-}
+    const input = kommenttikenttä.value;
+    const teksti = document.createTextNode(input);
+    const li = document.createElement("li");
+    const textdiv = document.createElement("div");
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Poista";
+    textdiv.appendChild(teksti);
+    li.appendChild(textdiv);
+    li.appendChild(deleteButton);
+    kommentit.appendChild(li);
+    deleteButton.onclick = function() {
+      deleteComment(deleteButton.parentElement);
+    };
+    kommenttikenttä.value = "";
+  }
+  
+  function deleteComment(event) {
+    event.remove();
+  }
+  
+  nappi.onclick = addComment;
 
 deleteButton.onclick = function() {
     deleteComment(deleteButton.parentElement);
